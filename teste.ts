16469bd2af5598c5a65150bf3c32ad09064fa9ed
Thais Leal <58890658+thaispll@users.noreply.github.com>
2020-10-24 13:48:40 -0300

    Add files via upload

---
+++ teste.ts
@@ -0,0 +1,12 @@
+function doSomething() {
+    console.log('did something')
+
+}
+
+function doSomethingINTS(name: string) void {
+    console.log(name) { name: 'Thais'}
+}
+
+doSomethingINTS(name:'Thais')
+
+doSomething()
